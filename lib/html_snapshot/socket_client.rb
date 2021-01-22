ed59d7b93099b43f377600ad971d0829f5eaa322
Thomas Cannon <tcannon00@gmail.com>
2021-01-22 12:36:06 -0500

    Fix initializing socket client

--- lib/html_snapshot/socket_client.rb
+++ lib/html_snapshot/socket_client.rb
@@ -4,10 +4,8 @@ require_relative 'socket_client/socket_connection_adapter'
 class HTMLSnapshot::SocketClient
   include HTTParty
 
-  attr_reader :socket_path
-
   def initialize(socket_path:)
-    @socket_path = socket_path
+    options[:socket_path] = socket_path
   end
 
   self.default_options = {connection_adapter: HTTParty::SocketConnectionAdapter}
