9cd34dacf7b9783cbedee40b559b90ff29bfa506
Thomas Cannon <tcannon00@gmail.com>
2021-12-10 14:20:24 -0500

    Add debug logging for `ScreenshotHTMLContent`

--- lib/html_snapshot/screenshot_html_content.rb
+++ lib/html_snapshot/screenshot_html_content.rb
@@ -1,9 +1,14 @@
 require 'terrapin'
+require 'logger'
 
 module HTMLSnapshot
   module ScreenshotHTMLContent
     def headless_browser_path
       raise NotImplemented
+    end
+
+    def logger
+      @logger ||= Logger.new(STDOUT)
     end
 
     def render_gif_of_html(html_content, window_size:)
@@ -14,16 +19,22 @@ module HTMLSnapshot
 
       temporary_html_file.write(html_content)
 
+      logger.debug(temporary_html_file: temporary_html_file.path, temporary_gif_file: temporary_gif_file.path)
+
       line = Terrapin::CommandLine.new(
         headless_browser_path,
         "--window-size=:window_size --disable-gpu --headless --hide-scrollbars --screenshot=:screenshot_path :local_html_path"
       )
 
-      line.run(
+      command_options = {
         window_size: window_size,
         screenshot_path: temporary_gif_file.path,
         local_html_path: temporary_html_file.path
-      )
+      }
+
+      line.run(command_options)
+
+      logger.debug(command: line.command(command_options))
 
       temporary_gif_file.rewind
       gif_content = temporary_gif_file.read
